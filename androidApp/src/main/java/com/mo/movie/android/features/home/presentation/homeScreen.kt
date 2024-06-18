a42416d33d5f1970469f3a8333f6c25e5e35ff91
Mohamed Emad <aleslam12345@gmail.com>
2024-06-18 03:04:47 +0300

    add custom collapsing toolbar

--- androidApp/src/main/java/com/mo/movie/android/features/home/presentation/homeScreen.kt
+++ androidApp/src/main/java/com/mo/movie/android/features/home/presentation/homeScreen.kt
@@ -1,5 +1,9 @@
 package com.mo.movie.android.features.home.presentation
 
+import android.annotation.SuppressLint
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,19 +12,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -36,9 +40,10 @@ import com.mo.movie.android.core.composables.Width
 import com.mo.movie.android.features.home.presentation.composables.HomeFilterButton
 import com.mo.movie.android.features.home.presentation.composables.MovieCard
 import com.mo.movie.android.theme.fontFamilyOverPass
+import com.mo.movie.core.utils.logit
 import com.mo.movie.features.home.presentation.HomeViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("ReturnFromAwaitPointerEventScope")
 @Composable
 fun HomeScreen(viewModel: HomeViewModel, navController: NavHostController) {
     LaunchedEffect(key1 = true) {
@@ -48,11 +53,26 @@ fun HomeScreen(viewModel: HomeViewModel, navController: NavHostController) {
     val context = LocalContext.current
     val moviesState = viewModel.moviesState.collectAsState()
     val filterState = viewModel.currentFilter.collectAsState()
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
-    Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            Column {
+    var isCollapsed by remember {
+        mutableStateOf(false)
+    }
+    val sizePercentage by remember {
+        mutableFloatStateOf(1f)
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier.animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessVeryLow
+                )
+            )
+        ) {
+            if (isCollapsed.not()) {
                 Height(10.dp)
                 ImagesSlider(
                     images = listOf(
@@ -63,54 +83,38 @@ fun HomeScreen(viewModel: HomeViewModel, navController: NavHostController) {
                     )
                 )
                 Height(5.dp)
-                TopAppBar(
-                    title = {
-                        Column {
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth(),
-                                horizontalArrangement = Arrangement.SpaceBetween,
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Text(
-                                    context.getString(R.string.trending),
-                                    modifier = Modifier.padding(
-                                        start = 8.dp,
-                                        end = 8.dp
-                                    ),
-                                    fontFamily = fontFamilyOverPass,
-                                    style = TextStyle(
-                                        color = MaterialTheme.colorScheme.onBackground,
-                                        fontSize = 18.sp
-                                    )
-                                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        context.getString(R.string.trending),
+                        modifier = Modifier.padding(
+                            start = 8.dp,
+                            end = 8.dp
+                        ),
+                        fontFamily = fontFamilyOverPass,
+                        style = TextStyle(
+                            color = MaterialTheme.colorScheme.onBackground,
+                            fontSize = 18.sp
+                        )
+                    )
 
-                                Row {
-                                    HomeFilterButton(
-                                        selectedFilter = filterState.value
-                                    ) { newFilter ->
-                                        viewModel.clear()
-                                        viewModel.getTrendingMovies(newFilter, CURRENT_LANGUAGE)
-                                    }
-                                    Width(width = 16.dp)
-
-                                }
-                            }
+                    Row {
+                        HomeFilterButton(
+                            selectedFilter = filterState.value
+                        ) { newFilter ->
+                            viewModel.clear()
+                            viewModel.getTrendingMovies(newFilter, CURRENT_LANGUAGE)
                         }
-                    },
-                    scrollBehavior = scrollBehavior,
-                    colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.background)
-                )
+                        Width(width = 16.dp)
+
+                    }
+                }
             }
         }
-    ) { padding ->
-        Column(
-            modifier = Modifier
-                .padding(top = padding.calculateTopPadding())
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
         Height(12.dp)
         HandleRequestStateUi(
             state = moviesState.value, modifier = Modifier
@@ -120,8 +124,37 @@ fun HomeScreen(viewModel: HomeViewModel, navController: NavHostController) {
             LazyVerticalGrid(
                 modifier = Modifier
                     .padding(horizontal = 12.dp)
-                    .fillMaxSize(),
-                columns = GridCells.Adaptive(minSize = 128.dp)
+                    .fillMaxSize()
+                    .pointerInput(Unit) {
+                        while (true) {
+                            val MIN_SCROLL_THRESHOLD = 40.dp
+                            var previousY = 0f
+                            val dragOffset = awaitPointerEventScope {
+                                val touchPoint = awaitPointerEvent()
+                                previousY = touchPoint.changes.last().previousPosition.y
+                                val event = awaitPointerEvent()
+                                val currentY = event.changes.last().position.y
+                                logit("previus : ${previousY}\ncurrentY : ${currentY}")
+                                currentY - previousY
+                            }
+                            if (Math.abs(dragOffset) > MIN_SCROLL_THRESHOLD.toPx()) {
+                                val scrollDirection = when {
+                                    dragOffset > 0 -> ScrollDirection.UP
+                                    dragOffset < 0 -> ScrollDirection.DOWN
+                                    else -> ScrollDirection.NONE
+                                }
+                                if (scrollDirection == ScrollDirection.UP) {
+                                    logit("up")
+                                    isCollapsed = false
+                                } else if (scrollDirection == ScrollDirection.DOWN) {
+                                    logit("down")
+                                    isCollapsed = true
+                                }
+                            }
+                        }
+                    },
+                columns = GridCells.Adaptive(minSize = 128.dp),
+
             ) {
                 items(count = movies.size) { index ->
                     val movie = movies[index]
@@ -134,5 +167,11 @@ fun HomeScreen(viewModel: HomeViewModel, navController: NavHostController) {
             }
         }
     }
+
 }
+
+enum class ScrollDirection {
+    UP,
+    DOWN,
+    NONE
 }
